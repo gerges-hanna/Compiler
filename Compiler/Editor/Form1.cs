f5031d9a772f52577f0a7c22aeea1bd396b8aa8c
Abanoub Rafaat <Abanoub_20180002@fci.helwan.edu.eg>
2021-05-27 02:55:08 +0200

    bebo

--- Compiler/Editor/Form1.cs
+++ Compiler/Editor/Form1.cs
@@ -16,5 +16,15 @@ namespace Compiler.Editor
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
